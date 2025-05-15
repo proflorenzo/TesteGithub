a1638b08205e8932f14387f9e3ffb21b8b960177
lorenzo <lorenzo.mr.rm@gmail.com>
2025-05-15 10:43:31 -0300

    Criadas novas Classes

---
+++ src/Teste/Alteracao.java
@@ -0,0 +1,9 @@
+package Teste;
+
+public class Alteracao {
+
+	public static void main(String[] args) {
+		System.out.println("Fizemos uma mudança");
+	}
+
+}
--- src/Teste/Main.java
+++ src/Teste/Main.java
@@ -6,5 +6,6 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Olá, github");
 		System.out.println("Versão atualizada");
+		System.out.println("Outra atualização");
 	}
 }
